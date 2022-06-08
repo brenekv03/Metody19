a798df113dbb12347c3cda17a144c3408acadbad
endurovojta173 <vojtechbrenek173@gmail.com>
2022-06-08 20:37:40 +0200

    Hotovo

--- Form1.cs
+++ Form1.cs
@@ -15,10 +15,41 @@ namespace Metody19
         public Form1()
         {
             InitializeComponent();
+
+        }
+        private static void Proved(ListBox listBox, int zadaneCislo)
+        {
+            int pc = 0;
+            int sc = 0;
+            for (int i = 0; i < listBox.Items.Count; i++)
+            {
+                int x = int.Parse(listBox.Items[i].ToString());
+                if (x % 2 == 0)
+                {
+                    pc++;
+                    sc += x;
+                    listBox.Items[i] = 0;
+                }
+                else
+                {
+                    x *= zadaneCislo;
+                    listBox.Items[i] = x;
+                }
+            }
+            double prumer = (double)sc / pc;
+            int indexprumer = listBox.Items.Count;
+            listBox.Items.Add(prumer);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Form1.Proved(listBox1, 5);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
-    private void Proved(ListBox listBox, int zadaneCislo)
-    {
-
-    }
+  
 }
